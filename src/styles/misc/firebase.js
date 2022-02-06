a787a8dec58e434e81b6a43c612cbea7355b00bf
Ayush13 <ranjanayush88@hotmail.com>
2022-02-07 01:52:03 +0530

    added firebase

---
+++ src/styles/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyApJZgKTOTxaBWSZ5Byuk5Jf9gLwLC6e5o',
+  authDomain: 'chatbox-51684.firebaseapp.com',
+  projectId: 'chatbox-51684',
+  storageBucket: 'chatbox-51684.appspot.com',
+  messagingSenderId: '590768763027',
+  appId: '1:590768763027:web:21261ea9eea6caa7159d66',
+};
+
+firebase.initializeApp(config);
