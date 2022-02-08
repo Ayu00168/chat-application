a8a21e3c7466ce9370043bcfa924a838a40093eb
Ayush13 <ranjanayush88@hotmail.com>
2022-02-08 18:03:56 +0530

    made our toggler responsive

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Button, Drawer, Icon } from "rsuite";
-import { useModalState } from "../../misc/custom-hook";
+import { useMediaQuery, useModalState } from "../../misc/custom-hook";
 import DashBoard from ".";
 const DashBoardToggle = () => {
   const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery("(max-width: 992px)");
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> DashBoard
       </Button>
-      <Drawer show={isOpen} onHide={close} palcement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} palcement="left">
         <DashBoard />
       </Drawer>
     </>
