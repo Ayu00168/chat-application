fe96578d80c4cc86092e1031f60415bae68874fc
Ayush13 <ranjanayush88@hotmail.com>
2022-02-09 02:10:20 +0530

    created on chage function

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Button, Drawer, Divider } from "rsuite";
+import { Button, Drawer, Divider, Alert } from "rsuite";
 import { useProfile } from "../../context/ProfileContex";
+import { database } from "../../misc/firebase";
 import EditableInput from "../EditableInput";
 
 const DashBoard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async (newData) => {
-    console.log("newData", newData);
+    const userNickNameRef = database
+      .ref(`/profile/${profile.uid}`)
+      .child("name");
+
+    try {
+      await userNickNameRef.set(newData);
+
+      Alert.success("Nickname has been Updated", 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
   };
   return (
     <>
