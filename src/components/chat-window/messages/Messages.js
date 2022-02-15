6f64e9f082b1ac096db82b4a227be758cf6ee971
Ayush13 <ranjanayush88@hotmail.com>
2022-02-15 23:11:04 +0530

    added delete message functionality and updated last message

--- src/components/chat-window/messages/Messages.js
+++ src/components/chat-window/messages/Messages.js
@@ -80,6 +80,39 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelete = useCallback(
+    async (msgId) => {
+      if (!window.confirm("Delete this message")) {
+        return;
+      }
+
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+      try {
+        await database.ref().update(updates);
+
+        Alert.info("Messages has been deleted");
+      } catch (err) {
+        Alert.error(err.message);
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li> NO Messages yet</li>}
@@ -90,6 +123,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelete}
           />
         ))}
     </ul>
