d93a33a5226a35cd72c4cb9451f227a298ae2ef2
Ayush13 <ranjanayush88@hotmail.com>
2022-02-15 21:56:36 +0530

    done like

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -7,6 +7,7 @@ import { Button } from "rsuite";
 import { useCurrentRoom } from "../../../context/currentRoomContext";
 import { auth } from "./../../../misc/firebase";
 import { useHover } from "../../../misc/custom-hook";
+import IconBtnControl from "./IconBtnControl";
 
 const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
@@ -48,6 +49,14 @@ const MessageItem = ({ message, handleAdmin }) => {
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(true ? { color: "red" } : {})}
+          isVisible
+          iconName="heart"
+          tooltip="Like this message..."
+          onClick={() => {}}
+          badgeContent={5}
         />
       </div>
       <div>
