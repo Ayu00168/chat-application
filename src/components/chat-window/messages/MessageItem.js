14ac77c27c39fbebf140a4dc1ddb37235e582371
Ayush13 <ranjanayush88@hotmail.com>
2022-02-16 19:40:24 +0530

    done voice recording

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -19,6 +19,14 @@ const renderFileMessage = (file) => {
       </div>
     );
   }
+  if (file.contentType.includes("audio")) {
+    return (
+      <audio controls>
+        <source src={file.url} type="audio/mp3" />
+        Mp3 is not suppoerted
+      </audio>
+    );
+  }
   return <a href={file.url}> Download {file.name}</a>;
 };
 
@@ -81,7 +89,7 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
             isVisible={canShowIcons}
             iconName="close"
             tooltip="Delete this message"
-            onClick={() => handleDelete(message.id)}
+            onClick={() => handleDelete(message.id, file)}
           />
         )}
       </div>
--- src/components/chat-window/messages/Messages.js
+++ src/components/chat-window/messages/Messages.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router";
 import { Alert } from "rsuite";
-import { auth, database } from "../../../misc/firebase";
+import { auth, database, storage } from "../../../misc/firebase";
 import { transformToArrWithId } from "../../../misc/helpers";
 import MessageItem from "./MessageItem";
 
@@ -81,7 +81,7 @@ const Messages = () => {
   }, []);
 
   const handleDelete = useCallback(
-    async (msgId) => {
+    async (msgId, file) => {
       if (!window.confirm("Delete this message")) {
         return;
       }
@@ -107,7 +107,16 @@ const Messages = () => {
 
         Alert.info("Messages has been deleted");
       } catch (err) {
-        Alert.error(err.message);
+        return Alert.error(err.message);
+      }
+
+      if (file) {
+        try {
+          const fileref = storage.refFromURL(file.url);
+          await fileref.delete();
+        } catch (err) {
+          Alert.error(err.message);
+        }
       }
     },
     [chatId, messages]
